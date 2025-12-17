62a8068a33c4d179ee4bbb74e59ed8ac14df8725
gerdinandvd <gerdinandvandam@gmail.com>
2025-12-18 00:52:21 +0100

    for modules also added a mapper

--- src/infrastructure/repositories/ModuleRepository.ts
+++ src/infrastructure/repositories/ModuleRepository.ts
@@ -4,28 +4,12 @@
 import { Module, IModuleRepository } from "@/domain";
 import { connectToDatabase } from "../database/mongodb";
 import { ModuleModel, IModuleDocument } from "../database/models/ModuleModel";
+import { ModuleMapper } from "../mappers/ModuleMapper";
 
 export class ModuleRepository implements IModuleRepository {
-  private mapToEntity(doc: IModuleDocument): Module {
-    return {
-      _id: doc._id.toString(),
-      id: doc.id,
-      name: doc.name,
-      shortdescription: doc.shortdescription,
-      description: doc.description,
-      studycredit: doc.studycredit,
-      location: doc.location,
-      level: doc.level,
-      learningoutcomes: doc.learningoutcomes,
-      estimated_difficulty: doc.estimated_difficulty,
-      available_spots: doc.available_spots,
-      start_date: doc.start_date,
-    };
-  }
-
   async findAll(): Promise<Module[]> {
     await connectToDatabase();
     const docs = await ModuleModel.find({});
-    return docs.map((doc) => this.mapToEntity(doc as IModuleDocument));
+    return docs.map((doc) => ModuleMapper.mapToDomain(doc as IModuleDocument));
   }
 }
