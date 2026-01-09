dd69ab36edb7f3b38649a63d850e11f04ef83eb0
gerdinandvd <gerdinandvandam@gmail.com>
2026-01-09 13:08:13 +0100

    removed unused methods and unnessicairy comments

--- src/application/services/ModuleService.ts
+++ src/application/services/ModuleService.ts
@@ -30,20 +30,4 @@ export class ModuleService {
     if (!modules) return null;
     return modules;
   }
-
-  async updateChosenModule(
-    user_id: string,
-    module_id: number,
-    chosen: boolean
-  ): Promise<boolean> {
-    if (chosen) {
-      return this.moduleRepository.addChosenModule(user_id, module_id);
-    } else {
-      return this.moduleRepository.pullChosenModule(user_id, module_id);
-    }
-  }
-
-  // async getMongoIdByModuleId(module_id: string): Promise<string | null> {
-  //   return this.moduleRepository.findMongoIdByModuleId(module_id);
-  // }
 }
